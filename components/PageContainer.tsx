821c20fefb7041989e8514eb90059117fff68180
Theeraphon Samathi <theeraphon.s@tgh.co.th>
2022-02-13 00:00:59 +0700

    add page container

---
+++ components/PageContainer.tsx
@@ -0,0 +1,11 @@
+import { styled, Paper } from '@mui/material'
+
+
+export const PageContainer = styled(Paper)({
+    minHeight: '200px',
+    marginTop: '20px',
+    marginBottom: '40px',
+    padding: '20px !important'
+});
+
+export default PageContainer;
