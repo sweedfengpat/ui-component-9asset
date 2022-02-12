b3a84978eb861a7de92cf11c731290c80a09b397
Theeraphon Samathi <theeraphon.s@tgh.co.th>
2022-02-12 23:07:59 +0700

    feat(hot action): add hot action

---
+++ components/HotActionBar.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@mui/material";
+import React from "react";
+import styled from "styled-components";
+
+const MainContainer = styled.div({
+    display: 'flex',
+    width: '100%'
+});
+
+export interface HotActionBarProps {
+    type: 'seller' | 'buyer';
+    onNewRequirementRequest?: () => void;
+}
+class HotActionBar extends React.Component<HotActionBarProps> {
+
+    public static defaultProps = {
+        type: 'buyer'
+    }
+
+    onCreateNew = () => {
+        if (this.props.onNewRequirementRequest) {
+            this.props.onNewRequirementRequest();
+        }
+    }
+
+    render () {
+        return (
+        <MainContainer>
+            { this.props.type === 'buyer' ? (
+                <>
+                    <Button variant="contained" color="primary" style={{ marginRight: 5, backgroundColor: '#ff5e5e' }} onClick={this.onCreateNew}>Create Requirement</Button>
+                    <Button variant="contained" color="secondary">Sell property/service</Button>
+                </>)  : (
+                <>
+                    <Button variant="contained" color="primary" style={{ marginRight: 5, backgroundColor: '#ff5e5e' }} onClick={this.onCreateNew}>Create Property</Button>
+                </>) }
+        </MainContainer>);
+    }
+}
+
+export default HotActionBar;
