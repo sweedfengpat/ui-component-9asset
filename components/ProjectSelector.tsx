7b1c97255cd0053f5e252c92bf4c9cb49a186f9e
Theeraphon Samathi <theeraphon.s@tgh.co.th>
2022-03-04 22:48:31 +0700

    feat(project): add project selector

---
+++ components/ProjectSelector.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export interface ProjectSelectorProps {
+
+}
+
+export interface ProjectSelectorState {
+
+}
+
+export default class ProjectSelecttor extends React.Component<ProjectSelectorProps, ProjectSelectorState> {
+    render(): React.ReactNode {
+        return (<></>);
+    }
+}
