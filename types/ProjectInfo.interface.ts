8515cd0214b5bb6b56bfdc398a9ef8a1a8828320
Theeraphon Samathi <theeraphon.s@tgh.co.th>
2022-08-24 16:54:32 +0700

    feat(project): change project info type

--- types/ProjectInfo.interface.ts
+++ types/ProjectInfo.interface.ts
@@ -1,15 +1,39 @@
 export type I18N = {
-    [key in keyof string]: string
+    TH: string;
+    EN: string;
 }
 
 export interface ProjectInfo {
-    name: I18N,
-    address: {
-        province?: string;
-        district?: string;
-        subdistrict?: string;
-        road?: string;
-        no?: string;
-        zipcode?: string;
-    }
+
+    name: I18N;
+    id?: number;
+    alley?: I18N;
+    road?: I18N;
+    type: number;
+    category?: {
+        id: number;
+        name: I18N;
+    };
+
+    developed?: I18N;
+    lat?: string | number;
+    lng?: string | number;
+    location?: I18N;
+
+    district?: {
+        id: number;
+        name: I18N;
+    };
+    subdistrict?: {
+        id: number;
+        name: I18N;
+    };
+    province?: {
+        id: number;
+        code?: string;
+        name: I18N;
+        lat?: string | number;
+        lng?: string | number;
+    };
+    status?: number;
 }
