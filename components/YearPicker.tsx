ce876802127a6aa402359232c00b01761afa1247
Theeraphon Samathi <theeraphon.s@tgh.co.th>
2023-03-07 01:32:33 +0700

    fix(year picker): allow user to enter value of number

--- components/YearPicker.tsx
+++ components/YearPicker.tsx
@@ -1,10 +1,10 @@
-import { Autocomplete, TextField } from "@mui/material";
+import { Autocomplete, AutocompleteRenderInputParams, TextField } from "@mui/material";
 import React from "react";
 
 export interface YearPickerProps  {
     label: string;
     name: string;
-    value: { value?: string, label?: string };
+    value: { value: string, label: string };
     onChange?: (value: { value?: string, label?: string } | null) => void;
     
     error?: boolean | undefined;
@@ -13,7 +13,8 @@ export interface YearPickerProps  {
 
 export interface YearPickerState {
     open: boolean;
-    value: { label?: string, value?: string } | null;
+    value: { label: string, value: string } | null;
+    inputValue: string | null;
 
     onChange?: ( value: { label?: string, value?: string } | null) => void;
 }
@@ -28,7 +29,12 @@ export class YearPicker extends React.Component<YearPickerProps, YearPickerState
         this.state = {
             open: false,
             value: this.props.value || null,
+            inputValue: this.props?.value?.value || null
         }
+    }
+
+    componentDidMount(): void {
+        this.setState({ inputValue: this.props?.value?.value || null });
     }
 
     createOptions = () => {
@@ -49,7 +55,9 @@ export class YearPicker extends React.Component<YearPickerProps, YearPickerState
     render(): React.ReactNode {
         return (
         <Autocomplete
-            renderInput={(params: any) => 
+            disablePortal
+            renderInput={(params: AutocompleteRenderInputParams) => {
+                return (
                 <TextField
                     {...params}
                     name={this.props.name}
@@ -59,27 +67,50 @@ export class YearPicker extends React.Component<YearPickerProps, YearPickerState
                     fullWidth
                     error={this.props.error}
                     helperText={this.props.helperText}
-                    // InputProps={{
-                    //     ...params.InputProps,
-                    //     endAdornment: (<>
-                    //         <InputAdornment position="end">
-                    //             { this.state.value ? <CloseOutlined /> : <></> }
-                    //             { this.state.open ? <ArrowDropUp /> : <CalendarTodayOutlined /> }
-                    //         </InputAdornment>
-                    //     </>),
-                    // }}
-                />
-            }
-            fullWidth
-            disablePortal
+                />)
+            }}
             options={this.options}
-            sx={{ display: 'inline-block' }}
-            onOpen={() => this.setState({ open: true })}
-            onClose={() => this.setState({ open: false })}
-            // value={this.state.value?.value || ''}
-            inputValue={this.state.value?.value || ''}
+
+            inputValue={this.state.inputValue || undefined}
+            onInputChange={(event, newInputValue) => {
+                this.setState({ inputValue: newInputValue });
+            }}
+
+            value={this.state.value}
             onChange={(e, v) => this.onSelectChanged(v)}
         />
+        // <Autocomplete
+        //     renderInput={(params: any) => 
+        //         <TextField
+        //             {...params}
+        //             name={this.props.name}
+        //             label={this.props.label}
+        //             variant="outlined"
+        //             size="small"
+        //             fullWidth
+        //             error={this.props.error}
+        //             helperText={this.props.helperText}
+        //             // InputProps={{
+        //             //     ...params.InputProps,
+        //             //     endAdornment: (<>
+        //             //         <InputAdornment position="end">
+        //             //             { this.state.value ? <CloseOutlined /> : <></> }
+        //             //             { this.state.open ? <ArrowDropUp /> : <CalendarTodayOutlined /> }
+        //             //         </InputAdornment>
+        //             //     </>),
+        //             // }}
+        //         />
+        //     }
+        //     fullWidth
+        //     disablePortal
+        //     options={this.options}
+        //     sx={{ display: 'inline-block' }}
+        //     onOpen={() => this.setState({ open: true })}
+        //     onClose={() => this.setState({ open: false })}
+        //     // value={this.state.value?.value || ''}
+        //     inputValue={this.state.value?.value || ''}
+        //     onChange={(e, v) => this.onSelectChanged(v)}
+        // />
         );
     }
 }
