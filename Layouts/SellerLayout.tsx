cad0993f9e7ba4118103bcedf2ac569b2f0e40de
Theeraphon Samathi <git@theeraphon.me>
2023-10-13 00:34:08 +0700

    fix(layout): fix error

--- Layouts/SellerLayout.tsx
+++ Layouts/SellerLayout.tsx
@@ -9,7 +9,7 @@ import { Outlet, useLocation } from "react-router-dom";
 const LayoutRoot = styled(Box)({
   display: 'flex'
 });
-const MainContainer = styled(Box)({
+const MainContainer = styled('main')({
   backgroundColor: '#f4f6f8',
   minHeight: '100vh',
   paddingTop: 0,
@@ -174,7 +174,7 @@ export const SellerLayout = (props: SellerLayoutProps) => {
         <DrawerMenu menu={drawerMenu} />
       </Drawer>
 
-      <MainContainer component="main" sx={{ p: { xs: 1, sm: 2 } }}>
+      <MainContainer sx={{ p: { xs: 1, sm: 2 } }}>
         <Toolbar />
         <Grid container sx={{ height: '42px', display: { xs: 'none', sm: 'block' } }}></Grid>
         <Outlet />
