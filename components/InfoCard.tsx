ae671a0a309f3db6611b4f4af4acfafa27d86fe1
Theeraphon S <git@theeraphon.me>
2023-10-22 02:37:40 +0700

    feat(dashboard): bind value from store

--- components/InfoCard.tsx
+++ components/InfoCard.tsx
@@ -1,15 +1,19 @@
-import { Paper, Typography } from "@mui/material";
+import { Box, CircularProgress, Paper, Typography, useMediaQuery, useTheme } from "@mui/material";
 import React from "react";
 
 interface InfoCardProps {
     data: string;
     title: string;
-    value: number;
+    value: string | number;
 
+    isLoading?: boolean;
     onClick?: (key: string) => void;
 }
 
 export const InfoCard = (props: InfoCardProps) => {
+
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
     return (
     <Paper
@@ -24,16 +28,19 @@ export const InfoCard = (props: InfoCardProps) => {
         elevation={1}
         onClick={() => { props.onClick && props.onClick(props.data) }}
     >
-        <Typography
-            component="div"
-            variant="h3"
-            sx={{ 
-                fontWeight: '400',
-                color: '#f4762a',
-                fontSize: { xs: '24px', sm: '40px' }
-            }}>
-            { props.value }
-        </Typography>
+        { props.isLoading ? 
+            <CircularProgress color="primary" size={isMobile ? 21 : 38 } /> :
+            <Typography
+                component="div"
+                variant="h3"
+                sx={{ 
+                    fontWeight: '400',
+                    color: '#f4762a',
+                    fontSize: { xs: '24px', sm: '40px' }
+                }}>
+                { `${props.value || ''}` }
+            </Typography>
+        }
         <Typography variant="caption" component="div" sx={{ mt: 0, fontSize: { xs: '10px', sm: '14px' } }}>
             { props.title }
         </Typography>
