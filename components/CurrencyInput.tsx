c696a79f78db35e130e6d1e405a9d48e83d6c2ad
Chakkraphan Racho <chakkraphan.racho@gmail.com>
2024-06-21 22:59:11 +0700

    [update] max min currency Input

--- components/CurrencyInput.tsx
+++ components/CurrencyInput.tsx
@@ -1,13 +1,55 @@
+import React, { useState } from 'react';
 import { BaseTextFieldProps, InputAdornment, TextField, TextFieldProps } from "@mui/material";
-import NumberFormat from "react-number-format";
+import NumberFormat, { NumberFormatValues } from "react-number-format";
 
 export interface CurrencyInputProps extends BaseTextFieldProps {
     unit?: string;
     dataValidateKey?: string; 
     decimalScale?: number; 
+    minValue?: number;
+    maxValue?: number;
+    updateValue?: any;
+    index?: number;
 }
 
 export const CurrencyInput = (props: CurrencyInputProps & TextFieldProps) => {
+
+    const [value, setValue] = useState<string | number | any>(props.value || '');
+    const [error, setError] = useState<string | null>(null);
+
+    const handleValueChange = (values: { value: string }) => {
+        setValue(values.value);
+        if (props.onChange) {
+            props.onChange({
+                ...values,
+                target: {
+                    ...values,
+                    value: values.value,
+                },
+            } as any);
+        }
+    };
+
+    const handleBlur = () => {
+        let updatedValue = Number(value);
+        let errorMessage = null;
+
+        if (props.minValue !== undefined && updatedValue < props.minValue) {
+            updatedValue = props.minValue;
+            errorMessage = `Value must be at least ${props.minValue}`;
+        }
+
+        if (props.maxValue !== undefined && updatedValue > props.maxValue) {
+            updatedValue = props.maxValue;
+            errorMessage = `Value must be at most ${props.maxValue}`;
+        }
+
+        setValue(updatedValue);
+        setError(errorMessage);
+        if(errorMessage  && props.updateValue && typeof props.index === 'number') {            
+            props.updateValue({target: {name:props.name, value:''}}, +props.index); // Call updateValue with event and index
+        }
+    };
 
     return (
     <NumberFormat
@@ -35,6 +77,10 @@ export const CurrencyInput = (props: CurrencyInputProps & TextFieldProps) => {
         decimalScale={props?.decimalScale || 0}
         value={props.value as any || ''}
         onChange={props.onChange}
+        onValueChange={handleValueChange}
+        onBlur={handleBlur}
+        error={!!error}
+        helperText={error}
     />
     );
 }
