2da67026276aa0bb23033ee0b2e0ca508afe7106
Theeraphon S <git@theeraphon.me>
2024-08-24 00:04:15 +0700

    fix(json); fix json parse

--- utils/index.ts
+++ utils/index.ts
@@ -1,31 +1,44 @@
 export const getUser = () => {
-  const user = JSON.parse(localStorage.getItem(`9asset.userinfo`) || '{}',);
-  if(user?.id){
-    return user;    
+  try {
+    const user = JSON.parse(localStorage.getItem(`9asset.userinfo`) || '{}',);
+    if(user?.id){
+      return user;    
+    }
+    return null;
+  } catch {
+    return null;
   }
-  return null;
 }
 
 export const getUserSync = () => {
   return new Promise<any|null>((resolve) => {
-    const user = JSON.parse(localStorage.getItem(`9asset.userinfo`) || 'null');
-    if(user?.id){
-        return resolve(user);    
+
+    try {
+      const user = JSON.parse(localStorage.getItem(`9asset.userinfo`) || 'null');
+      if(user?.id){
+          return resolve(user);    
+      }
+    } catch {
+      return null;
     }
 
     let cnt = 0;
     const looper = setInterval(() => {
-        const user = JSON.parse(localStorage.getItem(`9asset.userinfo`) || 'null');
-        console.log(user);
-        if(user?.id){
-            clearInterval(looper);
-            return resolve(user);    
-        }
+        try {
+          const user = JSON.parse(localStorage.getItem(`9asset.userinfo`) || 'null');
+          console.log(user);
+          if(user?.id){
+              clearInterval(looper);
+              return resolve(user);    
+          }
 
-        cnt++;
-        if (cnt > 50) {
-            clearInterval(looper);
-            return resolve(null);
+          cnt++;
+          if (cnt > 50) {
+              clearInterval(looper);
+              return resolve(null);
+          }
+        } catch {
+          return null;
         }
     }, 1000);
   })
